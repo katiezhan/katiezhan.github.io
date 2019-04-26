f5abc89545429f768679765c1be65a52c780e280
Katie Zhan <rqzhan@uwaterloo.ca>
2019-04-25 22:33:35 -0400

    deleted previous align header

--- js/script.js
+++ js/script.js
@@ -1,15 +1,3 @@
-function start(numModal) {
-  // alignHeader();
-  loadModal(numModal);
-}
-
-function alignHeader() {
-  var ref = document.getElementById("topLeft");
-  var pos = ref.getBoundingClientRect();
-  var x = pos.left + 4 + "px";
-  document.getElementById("projectHeader").style.left = x;
-}
-
 function loadModal(numModal) {
   for(var i = 1; i <= numModal; i++) {
     $('#modal' + i).iziModal();
